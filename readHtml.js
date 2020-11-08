4f854532be09e840b30fca486e9fe2498e5a5b33
n-nhutai <tai.nguyen@digicommercegroup.com>
2020-11-08 17:01:20 +0700

    comment from TainDigi

---
+++ readHtml.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+fs.readFile('test.html', 'utf8', (err, data) => {
+	if (err) {
+		console.error(err)
+		return
+	}
+	console.log(data)
+	console.log("--------------------")
+	parseHtmlToJson(data)
+});
+
+
+function parseHtmlToJson(data) {
+	const xml2js = require('xml2js');
+	xml2js.parseString(data, (err, result) => {
+		if (err) {
+			throw err;
+		}
+
+		// `result` is a JavaScript object
+		// convert it to a JSON string
+		const json = JSON.stringify(result, null, 4);
+		
+		// log JSON string
+		console.log(json);
+		fs.writeFile('htmlToJson.json', json, function(err) {
+			if (err) return console.log(err);
+		});
+//comment from TaiDigi
+
+	});
+}
+
